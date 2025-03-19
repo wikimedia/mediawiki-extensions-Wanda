f00d0c87948679d9adc2a8d9531f67980231719f
Sanjay Thiyagarajan <sanjayipscoc@gmail.com>
2025-03-19 16:47:44 +0530

    Fix the maintenance class

--- maintenance/ReindexAllPages.php
+++ maintenance/ReindexAllPages.php
@@ -5,6 +5,7 @@ namespace MediaWiki\Extension\Wikai\Maintenance;
 use Maintenance;
 use MediaWiki\Extension\Wikai\Hooks\PageIndexUpdater;
 use MediaWiki\MediaWikiServices;
+use Title;
 
 $IP = getenv( 'MW_INSTALL_PATH' );
 if ( $IP === false ) {
@@ -17,11 +18,23 @@ $maintClass = ReindexAllPages::class;
 class ReindexAllPages extends Maintenance {
 	public function execute() {
 		$dbr = MediaWikiServices::getInstance()->getDBLoadBalancer()->getConnection( DB_REPLICA );
-		$res = $dbr->select( 'page', [ 'page_title' ], [], __METHOD__ );
+		$res = $dbr->select( 'page', [ 'page_title', 'page_namespace' ], [], __METHOD__ );
+
 		foreach ( $res as $row ) {
-			$title = $row->page_title;
-			wfDebugLog( 'Chatbot', "Reindexing page => " . $title->getText() );
+			$title = Title::newFromText( $row->page_title, $row->page_namespace );
+			if ( !$title ) {
+				wfDebugLog( 'Chatbot', "Invalid title found in database: " . $row->page_title );
+				continue;
+			}
+
+			wfDebugLog( 'Chatbot', "Reindexing page => " . $title->getPrefixedText() );
+
 			$wikiPage = MediaWikiServices::getInstance()->getWikiPageFactory()->newFromTitle( $title );
+			if ( !$wikiPage ) {
+				wfDebugLog( 'Chatbot', "Failed to load WikiPage for " . $title->getPrefixedText() );
+				continue;
+			}
+
 			PageIndexUpdater::updateIndex( $title, $wikiPage );
 		}
 	}
